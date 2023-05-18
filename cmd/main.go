6c7fa7a14d93d5df0aac250a13b34e259038a740
Harsh Agrawal <averageworkspace@gmail.com>
2023-05-18 08:06:26 +0530

    Added Data Creation

--- cmd/main.go
+++ cmd/main.go
@@ -8,23 +8,21 @@ import (
 )
 
 func main() {
-	_, err := os.Stat("data")
+	_, err := os.Stat("./data")
 
-	if err == nil {
-
-	} else {
-
-		err := os.Mkdir("data", 0755)
+	if err != nil {
+		err := os.Mkdir("./data", 0755)
 		if err != nil {
-			fmt.Println("Something went wrong!")
-		} else {
-
-			http.HandleFunc("/status", handlers.StatusHandler)
-			http.HandleFunc("/api/create", handlers.CreateHandler)
-
-			// Start the server
-			fmt.Println("Server started on port 8080")
-			http.ListenAndServe(":8080", nil)
+			fmt.Println(err)
+			return;
 		}
 	}
+
+	http.HandleFunc("/status", handlers.StatusHandler)
+	http.HandleFunc("/api/create", handlers.CreateHandler)
+
+	// Start the server
+	fmt.Println("Server started on port 8080")
+	http.ListenAndServe(":8080", nil)
+
 }
--- handlers/createHandler.go
+++ handlers/createHandler.go
@@ -1,20 +1,66 @@
 package handlers
 
 import (
-	"fmt"
+	"encoding/json"
 	"io"
 	"net/http"
+	"os"
 )
 
 func CreateHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
-	} else {
-		body, err := io.ReadAll(r.Body)
-
-		if err == nil {
-			fmt.Println(body)
-		}
+		return
 	}
 
+	// Read the request body
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Create a variable of type map[string]interface{} to store the parsed JSON data
+	var jsonData map[string]interface{}
+
+	// Parse the JSON data into the 'jsonData' variable
+	err = json.Unmarshal(body, &jsonData)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if jsonData["collection"] == nil {
+		http.Error(w, "Invalid Request", http.StatusBadRequest)
+	} else if jsonData["data"] == nil {
+		http.Error(w, "Invalid Request", http.StatusBadRequest)
+	} else {
+		collectionName := jsonData["collection"].(string)
+		err := os.MkdirAll("./data/"+collectionName, 0755)
+		if err != nil {
+			http.Error(w, "Failed to create collection directory", http.StatusInternalServerError)
+			return
+		}
+	
+		filePath := "./data/" + collectionName + "/data.json"
+		file, err := os.Create(filePath)
+		if err != nil {
+			http.Error(w, "Failed to create data file", http.StatusInternalServerError)
+			return
+		}
+		defer file.Close()
+	
+		dataBytes, err := json.Marshal(jsonData["data"])
+		if err != nil {
+			http.Error(w, "Failed to serialize data", http.StatusInternalServerError)
+			return
+		}
+	
+		_, err = file.Write(dataBytes)
+		if err != nil {
+			http.Error(w, "Failed to write data to file", http.StatusInternalServerError)
+			return
+		}
+	}
+	
 }
