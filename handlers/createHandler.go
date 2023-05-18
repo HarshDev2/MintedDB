7e790c5402150730bcbb2b083e73c47f7674ef2c
Harsh Agrawal <averageworkspace@gmail.com>
2023-05-18 13:07:51 +0530

    Update Create Operation

--- handlers/createHandler.go
+++ handlers/createHandler.go
@@ -32,35 +32,91 @@ func CreateHandler(w http.ResponseWriter, r *http.Request) {
 
 	if jsonData["collection"] == nil {
 		http.Error(w, "Invalid Request", http.StatusBadRequest)
+		return
 	} else if jsonData["data"] == nil {
 		http.Error(w, "Invalid Request", http.StatusBadRequest)
-	} else {
-		collectionName := jsonData["collection"].(string)
-		err := os.MkdirAll("./data/"+collectionName, 0755)
-		if err != nil {
-			http.Error(w, "Failed to create collection directory", http.StatusInternalServerError)
-			return
-		}
-	
-		filePath := "./data/" + collectionName + "/data.json"
+		return
+	}
+
+	collectionName := jsonData["collection"].(string)
+	err = os.MkdirAll("./data/"+collectionName, 0755)
+	if err != nil {
+		http.Error(w, "Failed to create collection directory", http.StatusInternalServerError)
+		return
+	}
+
+	filePath := "./data/" + collectionName + "/data.json"
+
+	_, err = os.Stat(filePath)
+	if err != nil {
 		file, err := os.Create(filePath)
 		if err != nil {
 			http.Error(w, "Failed to create data file", http.StatusInternalServerError)
 			return
 		}
+
+		dataBytes, err := json.Marshal([]interface{}{jsonData["data"]})
+		if err != nil {
+			http.Error(w, "Failed to serialize data", http.StatusInternalServerError)
+			return
+		}
+
+		_, err = file.Write(dataBytes)
+		if err != nil {
+			http.Error(w, "Failed to write data to file", http.StatusInternalServerError)
+			return
+		}
+
+		defer file.Close()
+	} else {
+		file, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE, 0644)
+		if err != nil {
+			http.Error(w, "Failed to open data file", http.StatusInternalServerError)
+			return
+		}
 		defer file.Close()
 	
-		dataBytes, err := json.Marshal(jsonData["data"])
+		existingData, err := io.ReadAll(file)
+		if err != nil {
+			http.Error(w, "Failed to read data from file", http.StatusInternalServerError)
+			return
+		}
+	
+		var existingArray []interface{}
+		err = json.Unmarshal(existingData, &existingArray)
+		if err != nil {
+			http.Error(w, "Failed to parse existing data", http.StatusInternalServerError)
+			return
+		}
+	
+		existingArray = append(existingArray, jsonData["data"])
+	
+		newDataBytes, err := json.Marshal(existingArray)
 		if err != nil {
 			http.Error(w, "Failed to serialize data", http.StatusInternalServerError)
 			return
 		}
 	
-		_, err = file.Write(dataBytes)
+		err = file.Truncate(0)
+		if err != nil {
+			http.Error(w, "Failed to truncate data file", http.StatusInternalServerError)
+			return
+		}
+	
+		_, err = file.Seek(0, 0)
+		if err != nil {
+			http.Error(w, "Failed to seek data file", http.StatusInternalServerError)
+			return
+		}
+	
+		_, err = file.Write(newDataBytes)
 		if err != nil {
 			http.Error(w, "Failed to write data to file", http.StatusInternalServerError)
 			return
 		}
 	}
 	
+	
+	
+
 }
